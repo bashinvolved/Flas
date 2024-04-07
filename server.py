f6bdcb0d45df73bd697f7225cd74f9eb15c6f04a
Dan <123473138+bashinvolved@users.noreply.github.com>
2024-04-07 14:32:41 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def form_sample():
         return render_template("base.html")
     elif request.method == 'POST':
         database = sqlite3.connect("base.sqlite")
-        database.cursor().execute(f"INSERT INTO email(email) VALUES('{request.form["email"]}')")
+        database.cursor().execute(f"INSERT INTO email(email) VALUES('{request.form['phonenumber']}')")
         database.commit()
         return "Форма отправлена"
 
